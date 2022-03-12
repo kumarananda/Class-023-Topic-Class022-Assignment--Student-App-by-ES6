f409fe33d65be3dac1b17a84746b6f93f677faf6
kumarananda <coderanandakumar@gmail.com>
2022-03-13 01:59:06 +0600

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// get elenents 
+const staff_Add_Form = document.getElementById('staff_Add_Form');
+
+staff_Add_Form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let form_data = new FormData(e.target);
+    // console.log(form_data); //>>  FormData {  }
+
+    let data = Object.fromEntries(form_data.entries())
+    console.log(data); // Object { name: "", call: "", location: "", gender: "on", photo: "" } //browser
+    
+    
+})
+
+
+// 22: min
